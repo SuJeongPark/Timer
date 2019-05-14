1f3bdd3f1003a3b366258c484d9b79061dbdacbd
Sujeong Park <s2017s08@e-mirim.hs.kr>
2019-05-14 11:47:16 +0900

    #1 타이머 폼 디자인 추가

--- Timer/Form1.cs
+++ Timer/Form1.cs
@@ -7,10 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using Microsoft.VisualBasic;
+
 namespace Timer
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
